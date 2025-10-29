56af915eb5f9e84c171fd99457d5003615eed769
ludhiana <dan24nah@gmail.com>
2025-10-29 10:19:32 +0300

    feat : Ajout de fonctionnalité pour voir la liste des utilisateurs

--- backend/employees/views.py
+++ backend/employees/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from .serializers import UserSerializer
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -16,3 +17,17 @@ def create_user(request):
             return Response(UserSerializer(user).data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+    
+@api_view(['GET'])
+@permission_classes([AllowAny])
+# Récupérer la liste des utilisateurs
+def list_users(request):
+    if request.method == 'GET':
+        # Récupérer tous les utilisateurs
+        users = User.objects.all()
+        
+        # Sérialiser les données des utilisateurs
+        serializer = UserSerializer(users, many=True)
+        
+        # Retourner la réponse avec les données sérialisées
+        return Response(serializer.data)        
