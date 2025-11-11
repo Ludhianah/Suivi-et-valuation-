f75a9cabab5fff7a17fda58011a493e60867d601
ludhiana <dan24nah@gmail.com>
2025-11-11 11:43:17 +0300

    feat: Améliorez la mise en page du composant de connexion avec des styles d'arrière-plan et de centrage

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -1,7 +1,14 @@
 import { Button } from '@mantine/core';
 
 function Demo() {
-  return <Button variant="filled">Button</Button>;
+
+
+  return (
+    <div className='bg-amber-50 h-screen flex items-center justify-center'>
+      <Button variant="filled">Button</Button>
+    </div>
+  )
+
 }
 
 export default Demo;
