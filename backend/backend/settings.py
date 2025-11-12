b924063c695fae1e57c7f3815e23fcdcbf0f1d87
ludhiana <dan24nah@gmail.com>
2025-11-12 09:36:01 +0300

    configuration CORS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -149,3 +149,8 @@ SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'BLACKLIST_AFTER_ROTATION': True,
 }
+# configuration CORS
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+   
+]
